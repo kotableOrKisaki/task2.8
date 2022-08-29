3bb3fb8211da0ce0d812edc7723bae268e619dff
kotableOrKisaki <super_misha05@mail.ru>
2022-08-29 19:43:51 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "meow";
+            string input;
+            int tryCount = 0;
+
+            while (tryCount < 3)
+            {
+                Console.Write("Для доступа введите пароль:\t");
+                input = Console.ReadLine();
+
+                if (input == password)
+                {
+                    Console.WriteLine("Доступ открыт, проходите!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("А у вас доступа то и нет!");
+                    tryCount++;
+                }
+            }
+
+            Console.WriteLine("Охрана проводит вас на выход!");
+        }
+    }
+}
